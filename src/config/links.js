fe0afa23ae05f9a0387ea7d9666b5b7520622ab2
fuyl <longqingmiyu@163.com>
2023-03-19 13:30:28 +0800

    auth:long date:20230319 notes:提交全部项目信息，整个项目的相关的代码

---
+++ src/config/links.js
@@ -0,0 +1,9 @@
+const FriendLinks = [
+  {
+    title: "龙-钢琴 - DragonPiano",
+    desc: "自由钢琴 让你听见内心的声音",
+    url: "http://www.autopiano.cn"
+  }
+];
+
+export default FriendLinks;
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'hash',
+  routes: [
+    { path: '/', name: 'AutoPianoPC', component: function() { return import ('@/pages/AutoPianoPC')} },
+    { path: '/links', name: 'FriendLinks', component: function() { return import ('@/pages/FriendLinks')} },
+    { path: '/H5', name: 'AutoPianoH5', component: function() { return import ('@/pages/AutoPianoMobile')} },
+  ]
+})
